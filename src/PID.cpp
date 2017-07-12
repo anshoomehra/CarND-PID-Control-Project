3c119dd91aa242bae7af70334fccefa2214c2c36
Anshoo Mehra <anshoomehra@gmail.com>
2017-07-12 15:36:26 -0400

    Completed Project Ovjectives ..

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <iostream>
 
 using namespace std;
 
@@ -11,11 +12,59 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+
+	this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
+
+    prev_cte = 0.0;
+  
+    iter = 1;
+    index = 0; // 1-Kp, 2=Ki, 3=Kd
+    steps = 50;
+    eval_steps =100; // steps+eval_steps=2*N from the lesson
+    dp = {1.0,1.0,1.0};
+    tol = 0.01;
+    err = 0;
+    best_err = std::numeric_limits<double>::max(); // initialize to max double value
+    wait_flag = false;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte, double dt) {
+
+	if ( iter == 1) {
+		prev_cte = cte;
+	}
+
+	double diff_cte = cte - prev_cte;
+	p_error = cte;
+	i_error += cte;
+	//i_error += cte * dt;
+	d_error = diff_cte;
+	//d_error = diff_cte / dt;
+
+	prev_cte = cte;
+
+	int eval_a = iter % steps;
+	int eval_b = iter % (steps+eval_steps);
+
+	if ( eval_b > steps) {
+		err += cte * cte;
+	}
+
+	//cout <<  "iter: " << std::to_string(iter) << " Kp: " << Kp << " | Kd: " << Kd << " | Ki: " << Ki  << endl;
+	cout <<  ">>iter: " << std::to_string(iter) << " p_err: " << p_error << " | i_err: " << i_error << " | d_err : " << d_error  << endl;
+	if ( iter == (steps + eval_steps)) {
+		cout << "best err : " << err << endl;
+	}
+	iter++;
 }
 
 double PID::TotalError() {
+	double totalErr = -Kp * p_error  -Kd * d_error -Ki * i_error;
+	//cout << "TotalError : " << totalErr << endl;
+	return totalErr;
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,12 @@ constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
+//Placeholders 
+bool init;
+double prev_time;
+double dt;
+double throttle ;
+
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
@@ -34,6 +40,22 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  // First sucess run manual observations: pid.Init(0.1, 0.0001,  0.5);
+     // Almost smooth @ pid.Init(0.1, 0.0001,  0.6);
+      // Getting there @ pid.Init(0.1, 0.0001,  0.7);
+        // Perhaps we can improve corners a bit more @ pid.Init(0.1, 0.0002,  0.9);
+          // I guess I am hapy with  this !! pid.Init(0.09, 0.0002,  1.5);
+          // And this : pid.Init(0.1, 0.0002,  1.5);
+          // VStill touching yellow lines -- pid.Init(0.15, 0.0002,  2.0);
+          // Now with in yellow lines : pid.Init(0.20, 0.0002,  3.0);
+          // Bit jerky on sharp turns: pid.Init(0.20, 0.0003,  3.0);
+  // Let's stop here .. :-) & try Twiddle !!
+  pid.Init(0.20, 0.0003,  3.5);
+  //Initialize Other Variables
+  init  = false;
+  prev_time = 0.0;
+  dt = 0;
+  throttle = 0.2;
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -58,8 +80,28 @@ int main()
           * another PID controller to control the speed!
           */
           
+          double current_time = clock();
+
+          if ( ! init ) {
+            dt = 1;
+            init = true;
+          }
+          else {
+            dt = (current_time - prev_time)/CLOCKS_PER_SEC;
+          }
+          prev_time = current_time;
+
+          pid.UpdateError(cte, dt);
+          steer_value = pid.TotalError();
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+
+          //Throttle Offset
+          if ( (fabs(cte) > 1) && (throttle > 0.3))
+            throttle  -= 0.05;
+          else if ( throttle <= 0.4)
+              throttle += 0.01;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
